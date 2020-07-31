32e89def03553d60d06635e27cab981808ccc493
Larisa Smirnova <smirnova.larisa2020@yandex.ru>
2020-07-31 16:31:29 +0300

    fix: all bugs from revu

--- script.js
+++ script.js
@@ -2,40 +2,27 @@
 let formElement = document.querySelector('.popup');
 let profile = document.querySelector('.profile');
 
-let editButton = profile.querySelector('.profile__info__edit');
+let editButton = profile.querySelector('.profile-name__edit');
 let closePopupButton = formElement.querySelector('.popup__close');
 let savePopupButton = formElement.querySelector('.popup__save');
 
-let nameInput = formElement.querySelector('.popup__name'); 
-let jobInput = formElement.querySelector('.popup__about'); 
+let nameInput = formElement.querySelector('.popup__input_type_name'); 
+let jobInput = formElement.querySelector('.popup__input_type_about'); 
     
-let nameProfile = document.querySelector('.profile__info__name');
-let jobProfile = document.querySelector('.profile__info__about');
+let nameProfile = document.querySelector('.profile-name__title');
+let jobProfile = document.querySelector('.profile-info__about');
 
-nameInput.value = nameProfile.textContent;
-jobInput.value = jobProfile.textContent;
-
-function openPopup () {
+function popupOpenClose () {
+  if (formElement.classList.contains('popup_opened')){
+    formElement.classList.remove('popup_opened');
+  }
+  else {
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
   formElement.classList.add('popup_opened');
-  
+  }
 }
 
-function closePopup () {
-  formElement.classList.remove('popup_opened');
-}
-
-function savePopup () {
-  nameProfile.textContent = nameInput.value;
-    jobProfile.textContent = jobInput.value;
-    closePopup();
-}
-editButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup);
-savePopupButton.addEventListener('click', savePopup);
-
- 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
@@ -47,11 +34,13 @@ function formSubmitHandler (evt) {
     // Получите значение полей из свойства value
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
-
+    popupOpenClose();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', popupOpenClose);
+closePopupButton.addEventListener('click', popupOpenClose);
 
 
